0b0391b342fdc3944667f1d49e23ebd9150b66a9
Jesse Corrington <jesse.corrington@gmail.com>
2017-05-15 11:23:42 -0700

    turn off bitcoinj logging, make password optional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,23 @@
-
-
 public class Main {
     public static void main(String[] args) {
         BitcoinManager mgr = new BitcoinManager();
 
-        if (args.length < 2) {
+        if (args.length < 1) {
             System.out.println("Usage: <wallet-file> <password>");
             return;
         }
 
         String filename = args[0];
-        String password = args[1];
-
-        //String filename = System.getProperty("user.home") + "/dev" + "/bitcoinkit.wallet";
+        String password = args.length >= 2? args[1] : "";
 
         try {
             mgr.load(filename);
 
             String pk = mgr.getPubKey();
-            String sk = mgr.exportPrivateKey(password);
+            System.out.println("PK: " + pk);
 
-            System.out.print("PK: " + pk);
-            System.out.print("SK: " + sk);
+            String sk = mgr.exportPrivateKey(password);
+            System.out.println("SK: " + sk);
         }
         catch (Exception e) {
             System.out.print(e.toString());
